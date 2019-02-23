8ab80ee83b021a8586026d81c25d915e456fb282
ajerds <33637260+ajerds@users.noreply.github.com>
2019-02-23 11:41:33 -0500

    add Ingredient button fixed

--- app/src/main/java/com/callisto/foodfirst/AddRecipeActivity.java
+++ app/src/main/java/com/callisto/foodfirst/AddRecipeActivity.java
@@ -8,10 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+
 public class AddRecipeActivity extends AppCompatActivity {
 
     EditText name;
     EditText description;
+    EditText instructions;
 
     Button newIngredient;
     Button submit;
@@ -23,14 +31,50 @@ public class AddRecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
 
+        name = (EditText) findViewById(R.id.editText6);
+        description = (EditText) findViewById(R.id.editText7);
+        instructions = (EditText) findViewById(R.id.editText9);
+
         newIngredient = (Button) findViewById(R.id.button2);
+        submit = (Button) findViewById(R.id.button4);
+
+        recycler = (RecyclerView) findViewById(R.id.recyclerView3);
+
         newIngredient.setOnClickListener( new View.OnClickListener() {
+            // how to save information goes here
             @Override
             public void onClick(View v) {
                 startActivity( new Intent( AddRecipeActivity.this, AddIngredientActivity.class));
 
             }
         });
+        submit.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View k) {
+                //addInformationToDatabse( k );
+                openAddIngriedientActivity( k );
+
+            }
+        });
+
+    }
+    public void openAddIngriedientActivity( View v ) {
+        startActivity( new Intent( AddRecipeActivity.this, AddIngredientActivity.class));
+    }
+    public void addInformationToDatabse( View k) {
+
+        MongoClientURI uri  = new MongoClientURI("mongodb://ajerdman:FoodFirst10072@foodfirst-shard-00-00-aarbi.azure.mongodb.net:27017,foodfirst-shard-00-01-aarbi.azure.mongodb.net:27017,foodfirst-shard-00-02-aarbi.azure.mongodb.net:27017/test?ssl=true&replicaSet=FoodFirst-shard-0&authSource=admin&retryWrites=true");
+        MongoClient client = new MongoClient(uri);
+        MongoDatabase db = client.getDatabase(uri.getDatabase());
+        MongoCollection<Document> coll = db.getCollection("newDB");
+
+        Document doc = new Document( "item", "recipe")
+                .append( "name", name.getText().toString() )
+                .append( "description", description.getText().toString() );
+
+
+        coll.insertOne(doc);
+        client.close();
 
     }
 
