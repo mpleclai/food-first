9e76cf2ceb232974a471ece59a8f8f80ea06570e
ajerds <33637260+ajerds@users.noreply.github.com>
2019-02-23 01:34:52 -0500

    save data from add ingreedint

--- app/src/main/java/com/callisto/foodfirst/AddIngreedientActivity.java
+++ app/src/main/java/com/callisto/foodfirst/AddIngreedientActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -33,6 +34,21 @@ public class AddIngreedientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ingreedient);
+
+        ingreedientText = (EditText) findViewById(R.id.ingreedientText);
+        amountText = (EditText) findViewById(R.id.amountText);
+        caloriesText = (EditText) findViewById(R.id.caloriesText);
+
+        submit = (Button) findViewById(R.id.submit);
+
+        submit.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                ingreedient =  ingreedientText.getText().toString();
+                amount = Double.valueOf(amountText.getText().toString());
+                calories = Integer.valueOf(caloriesText.getText().toString());
+            }
+        });
     }
 
 }
