4c92cfaba61973c12a2cd4f0cce9aca303f253e7
ajerds <33637260+ajerds@users.noreply.github.com>
2019-02-23 02:47:26 -0500

    connect add recipe to add ingreedient

--- app/src/main/java/com/callisto/foodfirst/AddRecipeActivity.java
+++ app/src/main/java/com/callisto/foodfirst/AddRecipeActivity.java
@@ -1,14 +1,29 @@
 package com.callisto.foodfirst;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddRecipeActivity extends AppCompatActivity {
+
+
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+
+        button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent( AddRecipeActivity.this, AddIngreedientActivity.class));
+            }
+        });
     }
 
 }
